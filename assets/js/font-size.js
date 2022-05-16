8064753541c1e7ddf3c477e23e4b3f0099c8b6de
Conor Reynolds <conor.reynolds@mu.ie>
2022-05-16 18:09:26 +0100

    Add buttons for changing font size

---
+++ assets/js/font-size.js
@@ -0,0 +1,23 @@
+const fontSizeInc = document.querySelector('#font-inc');
+const fontSizeDec = document.querySelector('#font-dec');
+const fontSizeDefault = document.querySelector('#font-default');
+
+function changeFontSize(selector, amount) {
+    element = document.querySelector(selector);
+    style = window.getComputedStyle(element, null).getPropertyValue('font-size');
+    currentSize = parseFloat(style);
+    element.style.fontSize = (currentSize + amount) + 'px';
+}
+
+fontSizeInc.addEventListener('click', (_event) => {
+    changeFontSize('html', 1);
+});
+
+fontSizeDec.addEventListener('click', (_event) => {
+    changeFontSize('html', -1);
+});
+
+fontSizeDefault.addEventListener('click', (_event) => {
+    element = document.querySelector('html');
+    element.style.fontSize = "16px";
+});
