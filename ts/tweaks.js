a308053d00aaaee122060088d00cf56a11d28f2f
ConorReynolds <ConorReynolds@users.noreply.github.com>
2022-07-01 16:54:39 +0000

    deploy: d3c006dc99d28fe4852ce34795262ff31cdbd2a6

--- ts/tweaks.js
+++ ts/tweaks.js
@@ -3,4 +3,13 @@
   document.querySelectorAll("a.footnote-backref").forEach((element) => {
     element.innerHTML = `<i class="fa fa-chevron-up"></i>`;
   });
+  document.querySelectorAll("a.bibbase.bibbase_page.link").forEach((element) => {
+    let parent = element.parentElement;
+    element.remove();
+    if (parent !== null) {
+      parent.style.display = "flex";
+      parent.style.gap = "1em";
+      parent.innerHTML = parent.innerHTML.replace(/&nbsp;/g, "");
+    }
+  });
 })();
