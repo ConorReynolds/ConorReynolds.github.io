e4d716278d6ed2231c93f34601c3f151d43d94a2
Conor Reynolds <conor.reynolds@mu.ie>
2022-12-04 12:36:06 +0000

    Updates

--- js/utils.js
+++ js/utils.js
@@ -19,3 +19,30 @@ function copyAndConfirm(node, str) {
     node.replaceChildren(children);
   }, 1000.0);
 }
+
+// This is mainly for spans that are clickable. ARIA attributes need to be
+// correctly updated whenever the element is interacted with.
+function toggleAriaExpanded(node) {
+  node.setAttribute(
+    'aria-expanded',
+    node.getAttribute('aria-expanded') === 'true'
+      ? 'false'
+      : 'true'
+  );
+}
+
+// This is mainly for spans that are clickable. Normally buttons can simulate a
+// click by pressing Spacebar or Enter. We need to add this functionality back
+// if we're using an element that isn’t a button.
+function treatNodeAsButton(node) {
+  node.addEventListener('keydown', event => {
+    if (event.key === ' ' || event.key === 'Enter' || event.key === "Spacebar") {
+      event.preventDefault();
+      event.target.click();
+    }
+  });
+}
+
+document.querySelectorAll('span.tooltip').forEach(element => {
+  treatNodeAsButton(element);
+})
