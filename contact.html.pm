8e31c1ed8962c0412a636c3f02f5746f7ce8792f
Conor Reynolds <conor.reynolds@mu.ie>
2022-12-04 15:51:24 +0000

    Update contact page to mention LinkedIn

--- contact.html.pm
+++ contact.html.pm
@@ -4,4 +4,6 @@
 ◊(define-meta toc-title "Contact")
 ◊(define-meta subtitle "Email is best")
 
-The best way to contact me is by email at ◊extlink["mailto:conor.reynolds@mu.ie"]{conor.reynolds@mu.ie.} My social links, if you can call them that, extend only as far as a ◊extlink["https://github.com/ConorReynolds"]{GitHub account,} a ◊extlink["https://gitlab.cs.nuim.ie/creynolds"]{GitLab account,} and a ◊extlink["https://twitter.com/ConorEReynolds"]{Twitter account}---created against my better judgement.
+The best way to contact me is by email at ◊extlink["mailto:conor.reynolds@mu.ie"]{conor.reynolds@mu.ie.}
+
+My social links extend only as far as a ◊extlink["https://github.com/ConorReynolds"]{GitHub account,} a ◊extlink["https://gitlab.cs.nuim.ie/creynolds"]{GitLab account,} a ◊extlink["https://www.linkedin.com/in/conor-reynolds-931049258/"]{LinkedIn account,} and a ◊extlink["https://twitter.com/ConorEReynolds"]{Twitter account.}
