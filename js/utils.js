a219f60d1c26c22f2871fff91b1a1111040f6947
Conor Reynolds <conor.reynolds@mu.ie>
2022-12-05 00:09:59 +0000

    Updates

--- js/utils.js
+++ js/utils.js
@@ -8,8 +8,7 @@ var getAbsoluteUrl = function (url) {
   return getAbsoluteUrl(url);
 }
 
-function copyAndConfirm(node, str) {
-  navigator.clipboard.writeText(str);
+function confirm(node) {
   const children = node.querySelector('*');
   node.replaceChildren(new DOMParser().parseFromString(
     `<i class="fa fa-check"></i>`,
@@ -18,6 +17,11 @@ function copyAndConfirm(node, str) {
   setTimeout(function () {
     node.replaceChildren(children);
   }, 1000.0);
+}
+
+function copyAndConfirm(node, str) {
+  navigator.clipboard.writeText(str);
+  confirm(node);
 }
 
 // This is mainly for spans that are clickable. ARIA attributes need to be
@@ -43,6 +47,20 @@ function treatNodeAsButton(node) {
   });
 }
 
+function download(node, filename, text) {
+  var anchor = document.createElement('a');
+  anchor.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  anchor.setAttribute('download', filename);
+  anchor.style.display = 'none';
+
+  document.body.appendChild(anchor);
+  anchor.click();
+  document.body.removeChild(anchor);
+
+  confirm(node);
+}
+
 document.querySelectorAll('span.tooltip').forEach(element => {
   treatNodeAsButton(element);
-})
+});
+
