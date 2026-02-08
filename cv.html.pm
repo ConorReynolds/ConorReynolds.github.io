b8023542c1467d7c379441801777b1b6fe080c0a
Conor Reynolds <conor.reynolds@mu.ie>
2026-02-08 15:45:48 +0000

    Small updates

--- cv.html.pm
+++ cv.html.pm
@@ -2,46 +2,29 @@
 
 ◊(define-meta toc-title "CV")
 ◊(define-meta title "Curriculum Vitae")
-◊(define-meta subtitle "Under construction 🚧")
+◊(define-meta subtitle "Postdoctoral Researcher & Software Developer")
 
-◊section-ruled{Research Activities}
-
-My publications are on the ◊xref{home page}, if you're looking for those. Some other things I'm doing or have done:
-
-◊ul[#:compact #f]{  
-  ◊item{
-    I'm on the program committee for ◊extlink["https://smcit-scc.space/"]{IEEE SMC-IT 2024}. ◊aside{Pronounced 'smack it'. I think all conference names should be this amusing.}
-  }
-  ◊item{
-    I'm the web chair (that is, I made the website) for ◊extlink["https://ifm2024.cs.manchester.ac.uk/"]{iFM 2024}. It's built using ◊extlink["https://docs.racket-lang.org/pollen/"]{Pollen}, same as this site.
-  }
-}
-
-◊section-ruled{PhD Thesis}
-
-Supervised by Prof Rosemary Monahan, I put ◊extlink["https://iep.utm.edu/insti-th/"]{institution theory} in the ◊extlink["https://rocq-prover.org/"]{Rocq Prover}. ◊aside{Formerly known as the Coq proof assistant. You can read about the name change ◊extlink["https://rocq-prover.org/about#Name"]{on their website}.} You can see it ◊extlink["https://github.com/ConorReynolds/coq-institutions"]{on GitHub}.
-
-Institution theory studies logical systems in general using category theory. An institution is a mathematical object which is supposed to approximate a 'logical system', and so by studying institutions we hope to study logical systems in general.
-
-I encoded some of the general theory of institutions in Rocq and (more significantly) instantiated the theory to a few concrete logics, mostly first-order logic and its variants. I also constructed a trace semantics for Event-B as an institution and combined it with linear temporal logic as a duplex construction. ◊aside{This sounds a lot fancier than it is, but it's still neat.}
 
 ◊section-ruled{Work & Education}
 
-◊hang-list[#:compact #t]{
+◊hang-list[#:compact #f]{
   ◊item{
-    Postdoctoral Research Associate in ◊em{Computer Science}, University of Manchester, 2023–pres.
+    ◊em{Front-End Web Developer}◊(br)The Pandemonium Institute, Sep--Nov 2025
   }
   ◊item{
-    Adjunct Lecturer in ◊em{Computer Science}, Fuzhou University, Mar--May◊(nbsp)2023
+    ◊em{Postdoctoral Research Associate} in Computer Science◊(br)University of Manchester, 2023–2025
   }
   ◊item{
-    PhD in ◊em{Computer Science}, Maynooth University, 2019–2023
+    ◊em{Adjunct Lecturer} in Computer Science◊(br)Fuzhou University, Mar--May◊(nbsp)2023
   }
   ◊item{
-    MSc in ◊em{Mathematics}, Maynooth University, 2018–2019 (◊sc-form{h1})
+    ◊em{PhD in Computer Science}◊(br)Maynooth University, 2019–2023
   }
   ◊item{
-    BSc in ◊em{Computational Thinking}, Maynooth University, 2015–2018 (◊sc-form{h1})
+    ◊em{MSc in Mathematics}◊(br)Maynooth University, 2018–2019 (◊sc-form{h1})
+  }
+  ◊item{
+    ◊em{BSc in Computational Thinking}◊(br)Maynooth University, 2015–2018 (◊sc-form{h1})
   }
 }
 
@@ -64,3 +47,24 @@ I encoded some of the general theory of institutions in Rocq and (more significa
     ◊em{Turing Prize in Computer Science}, awarded in 2016 to the best first-year Computational Thinking student at Maynooth University.
   }
 }
+◊section-ruled{Research Activities}
+
+My publications are on the ◊xref{home page}, if you're looking for those. Some other things I'm doing or have done:
+
+◊ul[#:compact #f]{  
+  ◊item{
+    I'm on the program committee for ◊extlink["https://smcit-scc.space/"]{IEEE SMC-IT 2024}.
+  }
+  ◊item{
+    I'm the web chair (that is, I made the website) for ◊extlink["https://ifm2024.cs.manchester.ac.uk/"]{iFM 2024}. It's built using ◊extlink["https://docs.racket-lang.org/pollen/"]{Pollen}, same as this site.
+  }
+}
+
+◊section-ruled{PhD Thesis}
+
+Supervised by Prof Rosemary Monahan, I put ◊extlink["https://iep.utm.edu/insti-th/"]{institution theory} in the ◊extlink["https://rocq-prover.org/"]{Rocq Prover}. ◊aside{Formerly known as the Coq proof assistant. You can read about the name change ◊extlink["https://rocq-prover.org/about#Name"]{on their website}.} You can see it ◊extlink["https://github.com/ConorReynolds/coq-institutions"]{on GitHub}.
+
+Institution theory studies logical systems in general using category theory. An institution is a mathematical object which is supposed to approximate a 'logical system', and so by studying institutions we hope to study logical systems in general.
+
+I encoded some of the general theory of institutions in Rocq and (more significantly) instantiated the theory to a few concrete logics, mostly first-order logic and its variants. I also constructed a trace semantics for Event-B as an institution and combined it with linear temporal logic as a duplex construction. ◊aside{This sounds a lot fancier than it is, but it's still neat.}
+
--- index.html.pm
+++ index.html.pm
@@ -2,14 +2,18 @@
 
 ◊(define-meta toc-title "Home")
 ◊(define-meta title "Conor Reynolds")
-◊(define-meta subtitle "Researcher in Formal Methods")
+◊(define-meta subtitle "Postdoctoral Researcher & Software Developer")
 
-I was a postdoc at the ◊extlink["https://www.manchester.ac.uk/"]{University of Manchester} working on a variety of things. I'm part of the ◊extlink["https://autonomy-and-verification.github.io/"]{Autonomy and Verification group}.
+I was a postdoc at the ◊extlink["https://www.manchester.ac.uk/"]{University of Manchester} working on a variety of things. I was part of the ◊extlink["https://autonomy-and-verification.github.io/"]{Autonomy and Verification group}.
 
-I spent my PhD encoding the ◊extlink["https://iep.utm.edu/insti-th/"]{theory of institutions} in the ◊extlink["https://rocq-prover.org/"]{Rocq Prover}. ◊aside{Formerly known as the Coq proof assistant. You can read about the name change ◊extlink["https://rocq-prover.org/about#Name"]{on their website}.} (It's all ◊extlink["https://github.com/ConorReynolds/coq-institutions"]{on GitHub}.) There are six or so concrete logics encoded there as of this writing, depending on how you count it.
+For my PhD research, I encoded the ◊extlink["https://iep.utm.edu/insti-th/"]{theory of institutions} in the ◊extlink["https://rocq-prover.org/"]{Rocq Prover}.◊(nbsp)◊aside{Formerly known as the Coq proof assistant. You can read about the name change ◊extlink["https://rocq-prover.org/about#Name"]{on their website}.} (It's all ◊extlink["https://github.com/ConorReynolds/coq-institutions"]{on GitHub}.) There are six or so concrete logics encoded there as of this writing, depending on how you count it.
 
-I wrote lots of teaching resources while I was a PhD student at ◊extlink["https://www.maynoothuniversity.ie/"]{Maynooth}. You can see a small selection of them ◊xref["resources.html"]{here}.
+I taught a lot over my academic career and wrote lots of teaching resources while I was a PhD student at ◊extlink["https://www.maynoothuniversity.ie/"]{Maynooth University}. You can see a small selection of them ◊xref["resources.html"]{here}.
+
+Most recently, I contracted as a web developer for ◊extlink["https://bloodontheclocktower.com/"]{The Pandemonium Institute}. I got to design and build the brand-new ◊extlink["https://script.bloodontheclocktower.com/"]{Script Tool} for the social deduction game ◊extlink["https://boardgamegeek.com/boardgame/240980/blood-on-the-clocktower"]{Blood on the Clocktower}. It started off as a spare-time project to learn some front-end web dev and ended up becoming the official replacement for their old script tool.
 
 ◊(hr)
+
+Below are a list of peer-reviewed publications I've written and been involved with over the course of my academic career.
 
 ◊(bib-items)
